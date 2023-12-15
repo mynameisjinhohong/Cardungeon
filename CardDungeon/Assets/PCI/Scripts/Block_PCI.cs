f183f3a9624d6b6645959a4920e9c11cd86ddcfc
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2023-12-16 05:11:28 +0900

    타일 이미지 랜덤 구현
    타일 이미지 랜덤으로 나오게

--- CardDungeon/Assets/PCI/Scripts/Block_PCI.cs
+++ CardDungeon/Assets/PCI/Scripts/Block_PCI.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Block_PCI : TileObject_PCI
 {
+    public List<Sprite> sprites = new List<Sprite>();
+    private void Start()
+    {
+        int rand = Random.Range(0, sprites.Count);
+        spriteRenderer.sprite = sprites[rand];
+    }
     public override void OnDamaged(Player_HJH player)
     {
         Destroy(gameObject);
