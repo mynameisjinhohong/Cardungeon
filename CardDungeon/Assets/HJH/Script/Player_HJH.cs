97a1f890fd6223f65dd75bf5d71cdf3bc171cdee
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2023-12-16 03:33:35 +0900

    플레이어 데이터

---
+++ CardDungeon/Assets/HJH/Script/Player_HJH.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_HJH : MonoBehaviour
+{
+    public int hp;
+    public int mp;
+    public bool myPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getDamage(int damage)
+    {
+        if(hp- damage > 0)
+        {
+
+        }
+    }
+
+}
