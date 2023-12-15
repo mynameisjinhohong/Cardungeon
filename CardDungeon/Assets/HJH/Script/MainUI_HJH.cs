ba7719fac80283ca6dcae43a2d9643b558ac7781
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2023-12-16 05:24:02 +0900

    hpBar및 mpBar 적용

--- CardDungeon/Assets/HJH/Script/MainUI_HJH.cs
+++ CardDungeon/Assets/HJH/Script/MainUI_HJH.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainUI_HJH : MonoBehaviour
 {
+    public Image playerIcon;
+    public TMP_Text playerNickName;
+    public Image[] hpBar;
+    public Image[] mpBar;
+
+    public Sprite hpSprite;
+    public Sprite mpSprite;
+    public Sprite emptySprite;
+
+    public Player_HJH myPlayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +26,36 @@ public class MainUI_HJH : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ReNewHp()
+    {
+        for(int i =0; i<hpBar.Length; i++)
+        {
+            if(myPlayer.HP > i)
+            {
+                hpBar[i].sprite = hpSprite;
+            }
+            else
+            {
+                hpBar[i].sprite = emptySprite;
+            }
+        }
+
+    }
+
+    public void ReNewMp()
+    {
+        for (int i = 0; i < mpBar.Length; i++)
+        {
+            if (myPlayer.Mp > i)
+            {
+                mpBar[i].sprite = mpSprite;
+            }
+            else
+            {
+                mpBar[i].sprite = emptySprite;
+            }
+        }
     }
 }
