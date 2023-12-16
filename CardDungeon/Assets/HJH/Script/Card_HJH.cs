1b8f3e668c39df19faffcec3576888ecbcbaf0a8
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2023-12-16 22:58:42 +0900

    카드 이펙트 하드코딩 추가

--- CardDungeon/Assets/HJH/Script/Card_HJH.cs
+++ CardDungeon/Assets/HJH/Script/Card_HJH.cs
@@ -11,6 +11,9 @@ public class Card_HJH : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
     public int handIdx; //�ڵ忡�� ���° ī������
     public PlayerDeck_HJH playerDeck;
     public Vector2 defaultPos;
+    public GameObject cardEffect;
+    public GameObject tileEffect;
+    public List<GameObject> tileEffects;
     #region �巡�� �� ���
 
     void ChildRayCast(bool onOff)
@@ -24,6 +27,172 @@ public class Card_HJH : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
     {
         defaultPos = transform.position;
         ChildRayCast(false);
+        switch (Mathf.Abs(cardIdx))
+        {
+            case 1:
+                if (cardIdx < 0)
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x, playerPos.y + 2, 0);
+                }
+                else
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x, playerPos.y + 1, 0);
+                }
+                break;
+            case 2:
+                if (cardIdx < 0)
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x + 2, playerPos.y, 0);
+                }
+                else
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x + 1, playerPos.y, 0);
+                }
+                break;
+            case 3:
+                if (cardIdx < 0)
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tileEffect.transform.position = new Vector3(playerPos.x - 2, playerPos.y, 0);
+                }
+                else
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x - 1, playerPos.y, 0);
+                }
+                break;
+            case 4:
+                if (cardIdx < 0)
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x, playerPos.y -2, 0);
+                }
+                else
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x, playerPos.y - 1, 0);
+                }
+                break;
+            case 5:
+                if (cardIdx < 0)
+                {
+                    for(int i =-3; i<4; i++)
+                    {
+                        for(int j = -3; j<4; j++)
+                        {
+                            if(i==0&& j == 0)
+                            {
+                                continue;
+                            }
+                            GameObject tile = Instantiate(tileEffect);
+                            tileEffects.Add(tile);
+                            Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                            tile.transform.position = new Vector3(playerPos.x +i , playerPos.y + j, 0);
+                        }
+                    }
+
+                }
+                else
+                {
+                    for (int i = -2; i < 3; i++)
+                    {
+                        for (int j = -2; j < 3; j++)
+                        {
+                            if (i == 0 && j == 0)
+                            {
+                                continue;
+                            }
+                            GameObject tile = Instantiate(tileEffect);
+                            tileEffects.Add(tile);
+                            Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                            tile.transform.position = new Vector3(playerPos.x + i, playerPos.y + j, 0);
+                        }
+                    }
+                }
+                break;
+            case 6:
+                if (cardIdx < 0)
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x, playerPos.y + 1, 0);
+                    GameObject tile2 = Instantiate(tileEffect);
+                    tileEffects.Add(tile2);
+                    tile.transform.position = new Vector3(playerPos.x, playerPos.y + -1, 0);
+
+                }
+                else
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x, playerPos.y + 1, 0);
+                    GameObject tile2 = Instantiate(tileEffect);
+                    tileEffects.Add(tile2);
+                    tile.transform.position = new Vector3(playerPos.x, playerPos.y - 1, 0);
+                }
+                break;
+            case 7:
+                if (cardIdx < 0)
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x + 1, playerPos.y, 0);
+                    GameObject tile2 = Instantiate(tileEffect);
+                    tileEffects.Add(tile2);
+                    tile.transform.position = new Vector3(playerPos.x - 1, playerPos.y , 0);
+
+                }
+                else
+                {
+                    GameObject tile = Instantiate(tileEffect);
+                    tileEffects.Add(tile);
+                    Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                    tile.transform.position = new Vector3(playerPos.x + 1, playerPos.y, 0);
+                    GameObject tile2 = Instantiate(tileEffect);
+                    tileEffects.Add(tile2);
+                    tile.transform.position = new Vector3(playerPos.x - 1, playerPos.y, 0);
+                }
+                break;
+            case 8:
+                for(int i =-1; i < 2; i++)
+                {
+                    for(int j = -1; j <2; j++)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            continue;
+                        }
+                        GameObject tile = Instantiate(tileEffect);
+                        tileEffects.Add(tile);
+                        Vector3 playerPos = GamePlayManager.Instance.players[GamePlayManager.Instance.myIdx].transform.position;
+                        tile.transform.position = new Vector3(playerPos.x + i, playerPos.y + j, 0);
+                    }
+                }
+                break;
+        }
 
     }
     void IDragHandler.OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
@@ -33,6 +202,11 @@ public class Card_HJH : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
     }
     void IEndDragHandler.OnEndDrag(UnityEngine.EventSystems.PointerEventData eventData)
     {
+        for(int i =0; i< tileEffects.Count; i++)
+        {
+            Destroy(tileEffects[i]);
+        }
+        tileEffects.Clear();
         if (!EventSystem.current.IsPointerOverGameObject())
         {
             if (!playerDeck.UseCard(handIdx))
@@ -42,6 +216,7 @@ public class Card_HJH : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
             }
             else
             {
+                Instantiate(cardEffect, transform);
                 ChildRayCast(true);
             }
         }
