bc16cf117b638c6d4aedc310dcc3cc8753c69958
JinHo Hong <110793489+mynameisjinhohong@users.noreply.github.com>
2023-12-17 04:51:06 +0900

    소트 복구

--- CardDungeon/Assets/HJH/Script/GamePlayManager.cs
+++ CardDungeon/Assets/HJH/Script/GamePlayManager.cs
@@ -36,7 +36,21 @@ public class GamePlayManager : Singleton<GamePlayManager>
     IEnumerator WaitforGameStart()
     {
         yield return new WaitUntil(() => BackendManager.Instance.isLoadGame);
-
+        BackendManager.Instance.UserDataList.Sort((UserData lhs, UserData rhs) =>
+        {
+            if (int.Parse(lhs.playerToken) < int.Parse(rhs.playerToken))
+            {
+                return 1;
+            }
+            else if (lhs.playerToken == rhs.playerToken)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        });
         DataInit();
             
         yield return new WaitUntil(() => isDataCheck);
