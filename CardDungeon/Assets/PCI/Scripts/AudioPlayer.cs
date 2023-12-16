49a14036c748dd38cf3d7761f72d35218b88165a
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2023-12-16 23:33:26 +0900

    브금 실행

--- CardDungeon/Assets/PCI/Scripts/AudioPlayer.cs
+++ CardDungeon/Assets/PCI/Scripts/AudioPlayer.cs
@@ -1,9 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class AudioPlayer : Singleton<AudioPlayer>
 {
+    private static AudioPlayer instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(instance);
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // 이벤트 리스너 등록
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // 이벤트 리스너 제거
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.buildIndex == 0)
+        {
+            PlayBGM(0);
+        }
+        else
+        {
+            PlayBGM(2);
+        }
+    }
+
     public List<AudioClip> sfxClips = new List< AudioClip>();
     public List<AudioClip> bgmClips = new List<AudioClip>();
     public AudioSource sfxPlayer, bgmPlayer;
