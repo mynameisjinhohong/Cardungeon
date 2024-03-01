2e72ad07fed1d639d2aaaf069a7a65a8a8766412
JinHo Hong <ghddhksduq@naver.com>
2024-03-01 20:01:04 +0900

    튜토리얼 4장까지 제작

--- CardDungeon/Assets/HJH/Script/Tutorial_HJH.cs
+++ CardDungeon/Assets/HJH/Script/Tutorial_HJH.cs
@@ -1,13 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Tutorial_HJH : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] pages;
+    public TMP_Text pageText;
+    int idx = 0;
+    public int Idx
     {
-        
+        get
+        {
+            return idx;
+        }
+        set
+        {
+            idx = value;
+            for(int i =0; i < pages.Length; i++)
+            {
+                if(i == idx)
+                {
+                    pages[i].gameObject.SetActive(true);
+                }
+                else
+                {
+                    pages[i].gameObject.SetActive(false);
+                }
+            }
+            pageText.text = (idx + 1) + " / " + pages.Length;
+        }
+    }
+    // Start is called before the first frame update
+    public void OnEnable()
+    {
+        idx = 0;
     }
 
     // Update is called once per frame
