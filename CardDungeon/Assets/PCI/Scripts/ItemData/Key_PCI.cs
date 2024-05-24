234ee752595896aa445e6275e09c02c8039ac6e7
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2024-05-25 00:33:47 +0900

    열쇠 알림 수정
    인스펙터에서 문구 수정 가능하게

--- CardDungeon/Assets/PCI/Scripts/ItemData/Key_PCI.cs
+++ CardDungeon/Assets/PCI/Scripts/ItemData/Key_PCI.cs
@@ -6,13 +6,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Key", menuName = "Scriptable Object/Item/Create Key Data")]
 public class Key_PCI : ItemData_PCI
 {
+    [TextArea]
+    public string announceAll;
+    [TextArea]
+    public string announceMe;
     public override void OnInteracted(Player_HJH player)
     {
         player.Keys++;
         base.OnInteracted(player);
         if (player.Keys == 3)
         {
-            GamePlayManager.Instance.mainUi.toastMsgContainer.AddMessage("누군가가 열쇠를 모두 모았습니다!", 3.0f);
+            if (player.isMine)
+            {
+                GamePlayManager.Instance.mainUi.toastMsgContainer.AddMessage(announceMe, 3.0f);
+            }
+            else
+            {
+                GamePlayManager.Instance.mainUi.toastMsgContainer.AddMessage(announceAll, 3.0f);
+            }
         }
     }
 }
