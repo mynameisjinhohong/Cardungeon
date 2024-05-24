52052a20a770894c19ad8d602063ce19656c627c
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2024-05-25 00:28:24 +0900

    열쇠 3개 알림 추가

--- CardDungeon/Assets/PCI/Scripts/ItemData/Key_PCI.cs
+++ CardDungeon/Assets/PCI/Scripts/ItemData/Key_PCI.cs
@@ -10,5 +10,9 @@ public class Key_PCI : ItemData_PCI
     {
         player.Keys++;
         base.OnInteracted(player);
+        if (player.Keys == 3)
+        {
+            GamePlayManager.Instance.mainUi.toastMsgContainer.AddMessage("누군가가 열쇠를 모두 모았습니다!", 3.0f);
+        }
     }
 }
